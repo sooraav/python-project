2eef46630d7fcb222e7952eb20173ac634e9fecf
sooraav <purushothaman.sourav4@gmail.com>
2019-01-28 15:22:33 +0530

    error correction.

--- hack.py
+++ hack.py
@@ -1,6 +1,6 @@
 import csv
-with open('PCA_CDB_3202_F_Census.csv','rb') as f:
+with open('PCA_CDB_3202_F_Census.csv','rt',encoding="utf8") as f:
   reader=csv.reader(f)
   your_list=list(reader)
 
-print your_list
+print (your_list)
